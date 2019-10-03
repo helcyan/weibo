d3a0250f23dc3b2454c317dcb0007a529799a6b6
Helcyan <helcyan@163.com>
2019-10-03 17:26:16 +0800

    完成用户注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,10 +22,20 @@ class UsersController extends Controller
     //表单数据
     public function store(Request $request)
     {
+        //表单验证
         $this->validate($request, [
             'name'      =>      'required|max:50',
-            'password'  =>      'required|email|unique:users|max:255',      //unique:users 验证users表中email的唯一性
-            'email'     =>      'required|confirmed|min:6'                 //confirmed 验证两次输入的密码是否一致
+            'email'     =>      'required|email|unique:users|max:255',      //unique:users 验证users表中email的唯一性
+            'password'  =>      'required|confirmed|min:6'                 //confirmed 验证两次输入的密码是否一致
         ]);
+
+        //获取表单数据
+        $user = User::create([
+            'name'  =>  $request->name,
+            'email'  =>  $request->email,
+            'password'  =>  bcrypt($request->password),
+        ]);
+        session()->flash('success','欢迎！您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$user]); //->with('success','欢迎！您将在这里开启一段新的旅程~')
     }
 }
